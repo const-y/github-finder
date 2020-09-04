a12e89e3f05ce20e0010c3a1399e7a51929928bd
Konstantin Yarish <pm-mc@mail.ru>
2020-09-04 22:30:54 +0300

    fetching user list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar title="Github Finder" />
-      <div className="container">
-        <Users />
+class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({
+      loading: false,
+      users: res.data,
+    });
+  }
+
+  render() {
+    const { loading, users } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" />
+        <div className="container">
+          <Users users={users} loading={loading} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
